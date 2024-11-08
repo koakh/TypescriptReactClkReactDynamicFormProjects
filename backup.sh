226de348f58d3e2179c5082ed63c5f6fa812b621
mariomonteiro <mario.monteiro@critical-links.com>
2024-11-08 10:54:44 +0000

    reorganize folder typo roolUp

--- backup.sh
+++ backup.sh
@@ -6,9 +6,16 @@ FILE="$DIR/$DT.tgz"
 FILE_EXCLUDE=exclude.tag
 mkdir $DIR -p
 
-touch .bak/${FILE_EXCLUDE}
-touch my-app/node_modules/${FILE_EXCLUDE}
-touch my-react-form-package/node_modules/${FILE_EXCLUDE}
+touch .bak/${FILE_EXCLUDE}/node_modules/${FILE_EXCLUDE}
+touch NewRollUpBase/my-app/node_modules/${FILE_EXCLUDE}
+touch NewRollUpBase/my-react-form-package/node_modules/${FILE_EXCLUDE}
+touch NewRollUpClkReactDynamicForm/clk-react-dynamic-form/node_modules/${FILE_EXCLUDE}
+touch NewRollUpClkReactDynamicForm/clk-react-dynamic-form-consumer-app/node_modules/${FILE_EXCLUDE}
+touch ReactClkMicroPalFormToolsPoc/01-project-without-package/node_modules/${FILE_EXCLUDE}
+touch ReactClkMicroPalFormToolsPoc/02-project-with-package/node_modules/${FILE_EXCLUDE}
+touch ReactPublishReactComponentAsPackageToNpmPoc/dummy-counter/node_modules/${FILE_EXCLUDE}
+touch ReactPublishReactComponentAsPackageToNpmPoc/javascript/node_modules/${FILE_EXCLUDE}
+touch ReactPublishReactComponentAsPackageToNpmPoc/typescript/node_modules/${FILE_EXCLUDE}
 
 tar -zcvf $FILE \
 	--exclude-tag-all=${FILE_EXCLUDE} \
